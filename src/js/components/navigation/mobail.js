ee3f9a762b33557ce525b83f1cbadb89778fbd24
karisDev <karjaakirill@gmail.com>
2023-02-12 10:57:48 +0300

    spell check 2

--- src/js/components/navigation/mobail.js
+++ src/js/components/navigation/mobail.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { withRouter } from '@reyzitwo/react-router-vkminiapps';
+import React from "react";
+import { withRouter } from "@reyzitwo/react-router-vkminiapps";
 
-import {
-	Tabbar,
-	TabbarItem
-} from '@vkontakte/vkui';
-import { Icon28HomeOutline, Icon28Profile } from '@vkontakte/icons';
+import { Tabbar, TabbarItem } from "@vkontakte/vkui";
+import { Icon28HomeOutline, Icon28Profile } from "@vkontakte/icons";
 
-function MobailNavigation({ router }) {
+function MobileNavigation({ router }) {
+  function openView(view) {
+    let nowView = router.activeView;
+    router.toView(view);
 
-	function openView(view) {
-		let nowView = router.activeView
-		router.toView(view)
-		
-		if (view === nowView) {
-		  router.toHash(`${view}/base`)
-		}
-	}
+    if (view === nowView) {
+      router.toHash(`${view}/base`);
+    }
+  }
 
-	return(
-	    <Tabbar>
-	      <TabbarItem
-	        selected={router.activeView === 'home'}
-	        onClick={() => openView('home')}
-	        text='Главная'
-	      ><Icon28HomeOutline/></TabbarItem>
+  return (
+    <Tabbar>
+      <TabbarItem
+        selected={router.activeView === "home"}
+        onClick={() => openView("home")}
+        text="Главная"
+      >
+        <Icon28HomeOutline />
+      </TabbarItem>
 
-	      <TabbarItem
-	        data-id='profile'
-	        selected={router.activeView === 'profile'}
-					onClick={() => openView('profile')}
-	        text='Профиль'
-	      ><Icon28Profile/></TabbarItem>
-	    </Tabbar>
-	)
+      <TabbarItem
+        data-id="profile"
+        selected={router.activeView === "profile"}
+        onClick={() => openView("profile")}
+        text="Профиль"
+      >
+        <Icon28Profile />
+      </TabbarItem>
+    </Tabbar>
+  );
 }
 
-export default withRouter(MobailNavigation);
+export default withRouter(MobileNavigation);
